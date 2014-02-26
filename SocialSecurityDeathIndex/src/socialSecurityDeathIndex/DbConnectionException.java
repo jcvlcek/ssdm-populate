fe5236ca537c6ac963191c733fde0a436f965301
jcvlcek <vlcek@msn.com>
2014-02-25 21:41:57 -0600

    Added DbConnectionException class, thrown by methods of IDatabaseConnection

---
+++ SocialSecurityDeathIndex/src/socialSecurityDeathIndex/DbConnectionException.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package socialSecurityDeathIndex;
+
+/**
+ * @author Jim
+ *
+ */
+public class DbConnectionException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 272509893542434911L;
+
+	/**
+	 * 
+	 */
+	public DbConnectionException( String message ) {
+		super( message );
+	}
+
+}
