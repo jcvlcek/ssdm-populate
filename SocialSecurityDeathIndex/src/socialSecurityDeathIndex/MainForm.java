420f37d41ed0e6ebe4a26427d00f595044e8826a
Jim Vlcek <vlcek@msn.com>
2014-03-02 14:19:26 -0600

    Rearranged UI elements into separate Database and Master File groups Provided property setters to display Master File and Current Record

--- SocialSecurityDeathIndex/src/socialSecurityDeathIndex/MainForm.java
+++ SocialSecurityDeathIndex/src/socialSecurityDeathIndex/MainForm.java
@@ -8,12 +8,17 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 
 public class MainForm {
 
 	protected Shell shlSsdiDeathMaster;
 	private Text txtHostname;
 	private Combo cmbDatabaseType;
+	private Label mlblFileName;
+	private Label mlblCurrentRecord;
+	private static MainForm mDefaultWindow;
 
 	/**
 	 * Launch the application.
@@ -21,11 +26,16 @@ public class MainForm {
 	 */
 	public static void main(String[] args) {
 		try {
-			MainForm window = new MainForm();
-			window.open();
+			mDefaultWindow = new MainForm();
+			mDefaultWindow.open();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static MainForm getDefault()
+	{
+		return mDefaultWindow;
 	}
 
 	/**
@@ -52,7 +62,22 @@ public class MainForm {
 		shlSsdiDeathMaster.setSize(450, 300);
 		shlSsdiDeathMaster.setText("SSDI Death Master File Browser");
 		
-		Button btnConnect = new Button(shlSsdiDeathMaster, SWT.NONE);
+		Group grpDatabase = new Group(shlSsdiDeathMaster, SWT.NONE);
+		grpDatabase.setText("Database");
+		grpDatabase.setBounds(10, 10, 428, 109);
+		
+		cmbDatabaseType = new Combo(grpDatabase, SWT.NONE);
+		cmbDatabaseType.setBounds(10, 20, 125, 29);
+		cmbDatabaseType.setItems(new String[] {"MySQL", "SQL Server"});
+		cmbDatabaseType.setToolTipText("Database type");
+		cmbDatabaseType.setText("MySQL");
+		
+		txtHostname = new Text(grpDatabase, SWT.BORDER);
+		txtHostname.setBounds(141, 22, 100, 27);
+		txtHostname.setText("localhost");
+		
+		Button btnConnect = new Button(grpDatabase, SWT.NONE);
+		btnConnect.setBounds(247, 20, 91, 29);
 		btnConnect.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
@@ -60,16 +85,14 @@ public class MainForm {
 			}
 		});
 		btnConnect.setToolTipText("Connect to database");
-		btnConnect.setBounds(247, 10, 91, 29);
 		btnConnect.setText("Connect...");
 		
-		cmbDatabaseType = new Combo(shlSsdiDeathMaster, SWT.NONE);
-		cmbDatabaseType.setItems(new String[] {"MySQL", "SQL Server"});
-		cmbDatabaseType.setToolTipText("Database type");
-		cmbDatabaseType.setBounds(10, 10, 125, 29);
-		cmbDatabaseType.setText("MySQL");
+		Group grpMasterFile = new Group(shlSsdiDeathMaster, SWT.NONE);
+		grpMasterFile.setText("Master File");
+		grpMasterFile.setBounds(10, 125, 428, 89);
 		
-		Button btnOpen = new Button(shlSsdiDeathMaster, SWT.NONE);
+		Button btnOpen = new Button(grpMasterFile, SWT.NONE);
+		btnOpen.setBounds(10, 20, 91, 29);
 		btnOpen.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
@@ -77,12 +100,28 @@ public class MainForm {
 			}
 		});
 		btnOpen.setToolTipText("Open an SSDI Death Master File");
-		btnOpen.setBounds(347, 10, 91, 29);
 		btnOpen.setText("Open...");
 		
-		txtHostname = new Text(shlSsdiDeathMaster, SWT.BORDER);
-		txtHostname.setText("localhost");
-		txtHostname.setBounds(141, 10, 100, 27);
+		mlblFileName = new Label(grpMasterFile, SWT.NONE);
+		mlblFileName.setBounds(124, 32, 287, 17);
+		mlblFileName.setText("No file loaded...");
+		
+		Label lblCurrentRecordBanner = new Label(grpMasterFile, SWT.NONE);
+		lblCurrentRecordBanner.setBounds(10, 62, 108, 17);
+		lblCurrentRecordBanner.setText("Current record:");
+		
+		mlblCurrentRecord = new Label(grpMasterFile, SWT.NONE);
+		mlblCurrentRecord.setBounds(124, 62, 287, 17);
 
+	}
+	
+	public void setMasterFile( String sPath )
+	{
+		mlblFileName.setText(sPath);
+	}
+	
+	public void setCurrentRecord( String sRecord )
+	{
+		mlblCurrentRecord.setText(sRecord);
 	}
 }
