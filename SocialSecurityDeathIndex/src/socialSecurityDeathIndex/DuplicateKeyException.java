9d0f5c05794b1f0ed08f66af5c432f1d4454ca70
jcvlcek <vlcek@msn.com>
2014-03-05 13:02:34 -0600

    Added duplicate key exception class for benefit of database connection classes (thrown when attempt made to add an already-existing record)

---
+++ SocialSecurityDeathIndex/src/socialSecurityDeathIndex/DuplicateKeyException.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package socialSecurityDeathIndex;
+
+/**
+ * @author Jim
+ *
+ */
+public class DuplicateKeyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7635668495979131324L;
+
+	/**
+	 * 
+	 */
+	public DuplicateKeyException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public DuplicateKeyException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public DuplicateKeyException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public DuplicateKeyException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 * @param arg2
+	 * @param arg3
+	 */
+	public DuplicateKeyException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
